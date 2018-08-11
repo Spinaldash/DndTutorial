652434aba6835f1a752102b18ad463e75f01e8a3
MJ Perez <blueavenger@gmail.com>
2018-08-11 10:28:42 -0700

    creating data & processing it into state

---
+++ src/initial_data.js
@@ -0,0 +1,19 @@
+const initialData = {
+  tasks: {
+    'task-1': {id: 'task-1', content: 'Take out the garbage'},
+    'task-2': {id: 'task-2', content: 'Make your bed'},
+    'task-3': {id: 'task-3', content: 'Charge Phone'},
+    'task-4': {id: 'task-4', content: 'Cook dinger'},
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'to-do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+    }
+  },
+  // Facilitate reordering of data
+  columnOrder: ['column-1'],
+}
+
+export default initialData;
