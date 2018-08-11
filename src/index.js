74c140b99e2a4bd27fa5f0dcb3ab6e12c9030612
MJ Perez <blueavenger@gmail.com>
2018-08-11 15:07:25 -0700

    State management to persist list changes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,42 @@ class App extends React.Component {
 
   onDragEnd = result => {
     //This function is responsibile for updating the result
-  }
+    const { destination, source, draggableId } = result;
+
+    if(!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    // copy column.taskIds
+    const newTaskIds = Array.from(column.taskIds);
+    // Starting at this index, remove 1 item
+    newTaskIds.splice(source.index, 1);
+    // Add to this index
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      }
+    }
+
+    this.setState(newState);
+  };
 
 
   render() {
@@ -22,11 +57,11 @@ class App extends React.Component {
       <DragDropContext
         onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map((columnId) => {
-        const column = this.state.columns[columnId]
-        const tasks = column.taskIds.map(e => {
-          return this.state.tasks[e]
-        })
-        return <Column key={column.id} column={column} tasks={tasks} />;
+          const column = this.state.columns[columnId]
+          const tasks = column.taskIds.map(e => {
+            return this.state.tasks[e]
+          })
+          return <Column key={column.id} column={column} tasks={tasks} />;
         })}
       </DragDropContext>
     )
